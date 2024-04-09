c7c904b658b09e15589f88c4e9e2c1ea118c9b96
AbhinavDeshmukh123 <abhinavdeshmukhara@gmail.com>
2024-04-09 14:44:21 +0530

    first commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+const connectingString = 'mongodb+srv://abhinav:12345@cluster0.sx3heqx.mongodb.net/?retryWrites=true&w=majority'
 const db = async () => {
     try {
         mongoose.set("strictQuery", false)
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(connectingString)
         console.log("DB connected")
     } catch (error) {
         console.log("DB connection failed with error: ", error)
